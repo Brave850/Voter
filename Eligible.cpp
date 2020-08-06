4f00e2ec60d9f41203d03da6eb18f612947e28f6
Brave850 <67492957+Brave850@users.noreply.github.com>
2020-08-06 20:50:12 +0530

    Create Eligible c++
    This Program prints that whether you are eligible or not

---
+++ Eligible.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+
+{
+
+ 	int age;
+  
+ 	cout<<"Enter Your Age: ";
+  
+ 	cin>>age;
+  
+ 	cout<<"\n";
+  
+ 	if(age>=18)
+  
+ 		cout<<"You are Eligible"<<endl;
+    
+ 	else
+  
+ 		cout<<"You are not Eligible"<<endl;
+    
+ 	return 0;
+  
+}
